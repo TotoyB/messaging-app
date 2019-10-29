380e0313ec1c5360f48071d1f8cdef6a7862aec2
TotoyB <tadeoalwilson.pfs@gmmail.com>
2019-10-29 19:02:22 +0800

    Create message table and add new message with a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: {case_sensitive: false},
             length: {minimum: 3, maximum: 20}
   has_secure_password
+  has_many :messages
 
 end
